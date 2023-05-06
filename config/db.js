18616932a76f7500a18b340cf1f52bc5859615b4
kazinayeem <mohammadalinayeem360@gmail.com>
2023-05-06 15:59:27 +0600

    first commits

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
 let localhost = "mongodb://localhost:27017/mywebsite";
+const dbs = "mongodb+srv://kazinayeem55085:8hxvuD4JpsadO87w@cluster0.06x25aj.mongodb.net/?retryWrites=true&w=majority"
 
+
+const pass = "8hxvuD4JpsadO87w"
 const db = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL);
+    await mongoose.connect(dbs);
     console.log("database connect");
   } catch (error) {
     console.log(error);
